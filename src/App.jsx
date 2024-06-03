4b248e654381c87a2beb10a9851cff1d5f6b21a2
silvaPool <silva.friends22@gmail.com>
2024-06-03 08:36:05 -0300

    Router implementado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { Switch } from '@mui/material'
 import './App.css'
 import LoginButton from './components/LoginButton'
+import { Route } from 'react-router-dom'
+import Buda from './pages/Buda'
+import Adao from './pages/Adao'
 
 function App() {
-
 
   return (
     <>
 
-    <LoginButton />
+        <Switch>
+          <Route path="/" element={LoginButton} />
+          <Route path="/buda" element={Buda} />
+          <Route path="/adao" element={Adao} />
+        </Switch>
      
     </>
   )
